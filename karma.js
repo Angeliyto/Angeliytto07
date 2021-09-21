a64b87223c2c696256a47f4f7318ce6693a80099
Angeliyto <89996464+Angeliyto@users.noreply.github.com>
2021-09-20 19:09:45 -0500

    Add files via upload

---
+++ karma.js
@@ -0,0 +1,23 @@
+var x = 0;
+for (var x = 0; x<=18; x++){
+    var total = x*.5;
+    console.log(total);
+}
+console.log("  ");
+var y = 100;
+for (var y = 100; y<=199; y++){
+    console.log(y);
+}
+console.log("  ");
+var z = 5;
+for(var z =5; z<=20; z++){
+        console.log(z);
+}
+console.log("  ");
+var i = 0;
+for (var i = 1; i<=100; i++){
+    for (var u = 1; u<=100; u++){
+        console.log(i + "+" + u + "=" + (i+u));
+    }
+}
+
